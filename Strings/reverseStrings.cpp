eb3e98899a567437ae42dad53f26778cbba467fe
sernat7 <edgar_serna717@hotmail.com>
2018-06-12 00:25:38 -0500

    reverse string

---
+++ Strings/reverseStrings.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    string reverseString(string s) {
+        for (int i = 0; i < s.length()/2; i++)
+        {
+           swap(s[i], s[s.length() -1 -i]);
+        }
+        return s;
+    }
+};
